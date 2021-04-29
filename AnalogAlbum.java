7e6f0d7e20896d78133a8d21c1885f4df9d3ddc5
rosesatriano <satrianr@email.sc.edu>
2021-04-28 23:08:11 -0400

    adding methods

--- AnalogAlbum.java
+++ AnalogAlbum.java
@@ -1,3 +1,14 @@
+
+
 public interface AnalogAlbum {
     
+    public String play();
+
+    public String rewind();
+
+    public String ffwd();
+
+    public String pause();
+
+    public String stopEject();
 }
--- Cassett.java
+++ Cassett.java
@@ -1,3 +1,23 @@
-public class Cassett {
-    
+
+
+public class Cassett implements AnalogAlbum {
+
+    private ArrayList<String> songs;
+    private int currentIndex;
+
+    public Cassett(String){
+
+    }
+
+    public String play();
+
+    public String rewind();
+
+    public String ffwd();
+
+    public String pause();
+
+    public String stopEject();
+
+
 }
